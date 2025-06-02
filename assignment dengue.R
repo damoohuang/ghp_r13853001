0224270a61d485bf797d6ee35617822cd2a72182
damoohuang <r13853001@g.ntu.edu.tw>
2025-06-02 18:13:26 +0800

    Update assignment dengue.R
    edit 2

--- assignment dengue.R
+++ assignment dengue.R
@@ -1,5 +1,4 @@
 library(dplyr)
 data<-read.csv("/Users/dianehuang/Downloads/ghp_r13853001/dengue_assignment.csv")
-data14<- filter(data,year==2014)
-total_cases<- nrow(data14)
-print(total_cases)
+data_2014 <- data[data$year == 2014, ]
+total_cases_2024 <- sum(data_2014$case_number, na.rm = TRUE)
